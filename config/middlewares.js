d89e64b2aaa93410f60619660933f4623bbdad3f
Leandro Medeiros <leandro.medeiros@versatilnow.com>
2023-12-13 18:59:09 -0300

    test

--- config/middlewares.js
+++ config/middlewares.js
@@ -21,15 +21,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "256mb", // modify form body
-      jsonLimit: "256mb", // modify JSON body
-      textLimit: "256mb", // modify text body
-      formidable: {
-        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
-      },
-    },
-  },
 ];
